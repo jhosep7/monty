fc8b3c5c4b66704f209c4afd92d889b85cc55547
andreammgcol <amendez72@misena.edu.co>
2020-01-04 23:06:07 +0000

    adition func swap - add

--- monty.h
+++ monty.h
@@ -21,9 +21,9 @@
  */
 typedef struct stack_s
 {
-int n;
-struct stack_s *prev;
-struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -36,8 +36,8 @@ struct stack_s *next;
  */
 typedef struct instruction_s
 {
-char *opcode;
-void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /**
@@ -48,7 +48,7 @@ void (*f)(stack_t **stack, unsigned int line_number);
  */
 typedef struct ShareVar
 {
-int intvar;
+	int intvar;
 } ShareVar_t;
 
 extern ShareVar_t SharedVar;
@@ -66,6 +66,8 @@ void op_push(stack_t **stack, unsigned int num);
 void op_pall(stack_t **stack, unsigned int num);
 void op_pint(stack_t **stack, unsigned int lnum);
 void op_pop(stack_t **stack, unsigned int num);
+void op_swap(stack_t **stack, unsigned int num);
+void op_add(stack_t **stack, unsigned int num);
 
 void FrStack(stack_t **stack, unsigned int lnum);
 
